a986069d9ebd20b183816cb75ab7738d71f05ba4
anands-cuelogic <anand.singh@cuelogic.com>
2018-09-06 21:42:32 +0530

    Implemented redis for cache the data

--- config/cache.js
+++ config/cache.js
@@ -1,8 +1,34 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import redis from 'redis';
+import util from 'util';
+
+const redisUrl = 'redis://localhost:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.exec = function(){
-    console.log('I am about to execute');
+mongoose.Query.prototype.exec = async function () {
 
-    return exec.apply(this, arguments);
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    }));
+
+    //See if we have a value for 'key' in redis
+    const cacheValue = await client.get(key);
+
+    //If we do return that
+    if (cacheValue) {
+        const doc = new this.model(JSON.parse(cacheValue));
+       return JSON.parse(cacheValue);
+    }
+
+    //Otherwise, issue the query and store the result in redis
+    const result = await exec.apply(this, arguments);
+    const check = client.set(key, JSON.stringify(result), (err, res) =>{
+        if(err) throw err;
+        
+    });
+    
+    return result;
 };
